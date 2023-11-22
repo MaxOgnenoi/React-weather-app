1513e090cd44944030d8bab7dcc362b410ac3896
Max Ognenoi <maksymohnenyi>
2023-11-21 22:53:44 -0500

    styling 5

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
 const PORT = process.env.PORT || 3012;
 
 // Serve static files from the build folder
