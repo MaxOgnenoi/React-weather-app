b6be48bb93580dadb2fd7209b0229c873d5ea9da
Max Ognenoi <maksymohnenyi>
2023-11-22 23:39:52 -0500

    style8

---
+++ src/components/WeatherFiveDayForecast.js
@@ -0,0 +1,31 @@
+import React from "react";
+import WeatherCard from "./WeatherCard";
+import "../styles/WeatherForecast.css";
+
+function WeatherForecast({ forecastData }) {
+  // Get the first 5 days of forecast
+  const forecastList = forecastData.list.slice(0, 5);
+
+  return (
+    <div className="forecast-container">
+      <h2>5-Day Forecast</h2>
+      <div className="forecast-cards">
+        {forecastList.map((forecast) => {
+          const date = new Date(forecast.dt * 1000);
+          const dayOfWeek = new Intl.DateTimeFormat("en-US", { weekday: "short" }).format(date);
+          
+          return (
+            <WeatherCard
+              key={forecast.dt}
+              weatherData={forecast}
+              showDate
+              dayOfWeek={dayOfWeek}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default WeatherForecast;
